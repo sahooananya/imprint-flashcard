1621638ef55672f18ff1d67150e561128148bf6a
sahooananya <126280248+sahooananya@users.noreply.github.com>
2026-04-11 13:40:34 +0530

    css transitions, config fixes

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['pdf-parse', 'jszip'],
   },
+  api: {
+    bodyParser: {
+      sizeLimit: '10mb',
+    },
+  },
 };
 
 module.exports = nextConfig;
